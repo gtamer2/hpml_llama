735a32aec92d6fd2f997f5fbfcbe7916cd1777fd
George Tamer <gtamer2@gmail.com>
2023-12-16 20:14:27 -0500

    Scripting

--- high_perf/optimize/scripting/script_model.py
+++ high_perf/optimize/scripting/script_model.py
@@ -11,29 +11,6 @@ def get_model(ckpt_dir, tokenizer_path, max_seq_len, max_batch_size):
     )
     return generator
 
-# def script_transformer(ckpt_dir, 
-#               tokenizer_path, 
-#               max_seq_len, 
-#               max_batch_size):
-#     print("Starting up...")
-#     torch.cuda.empty_cache()
-
-#     print("Initializing Model...")
-#     llama = get_model(ckpt_dir, tokenizer_path, max_seq_len, max_batch_size)
-#     transformer = llama.model
-#     transformer.eval()
-#     with torch.no_grad():
-#         print("Attempting to script model...")
-#         scripted_former = torch.jit.script(transformer)
-
-        
-#         llama.model = scripted_former
-#         print("Successfully scripted the transformer blocks of the model!")
-        
-#         print("Saving scripted model...")
-#         llama.save("scripted_llama.pt")
-
-
 def script_transformer_blocks(ckpt_dir, 
               tokenizer_path, 
               max_seq_len, 
@@ -51,10 +28,15 @@ def script_transformer_blocks(ckpt_dir,
         print(f"Scripting transformer block {idx}/{len(transformer_blocks)}...")
         transformer_block.eval()
         # scripted_block = torch.jit.script(transformer_block)
+        print("Scripting attention.wq")
         transformer_block.attention.wq = torch.jit.script(transformer_block.attention.wq)
+        print("Scripting attention.wk")
         transformer_block.attention.wk = torch.jit.script(transformer_block.attention.wk)
+        print("Scripting attention.wv")
         transformer_block.attention.wv = torch.jit.script(transformer_block.attention.wv)
+        print("Scripting attention.wo")
         transformer_block.attention.wo = torch.jit.script(transformer_block.attention.wo)
+        print("Scripting attention_norm")
         transformer_block.attention_norm = torch.jit.script(transformer_block.attention_norm)
 
     print("Successfully scripted the transformer blocks of the model!")
